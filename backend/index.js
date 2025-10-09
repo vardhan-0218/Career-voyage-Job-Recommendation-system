903fd3acb19dea0df014bc257ad9e83c2f95e547
vardhan <kvvardhan@05gmail.com>
2025-10-09 21:28:55 +0530

    index changed

--- backend/index.js
+++ backend/index.js
@@ -17,9 +17,13 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
+  origin: [
+    'http://localhost:5173',           // local development
+    'https://career-voyage.onrender.com'  // your deployed frontend
+  ],
+  credentials: true,
+};
+
 
 app.use(cors(corsOptions));
 
